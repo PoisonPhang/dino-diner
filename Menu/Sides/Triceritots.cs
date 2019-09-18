1a008d353af62d33349e5685dcb89ddfceb6bfc4
Connor Davis <connormdavis99@users.campus>
2019-09-18 17:06:08 -0500

    Added Triceritots

---
+++ Menu/Sides/Triceritots.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class Triceritots : Side
+    {
+        public override Size Size
+        {
+            get => Size;
+            set
+            {
+                this.Size = value;
+                switch(value)
+                {
+                    case Size.Large:
+                        this.Price = 1.95;
+                        this.Calories = 590;
+                        break;
+                    case Size.Medium:
+                        this.Price = 1.45;
+                        this.Calories = 610;
+                        break;
+                    default:
+                        this.Price = 0.99;
+                        this.Calories = 352;
+                        break;
+                }
+            }
+        }
+
+        public Triceritots()
+        {
+            this.Size = Size.Small;
+            this.Price = 0.99;
+            this.Calories = 352;
+            this.Ingredients.AddRange(new List<string> { "Potato", "Salt", "Vegetable Oil" });
+        }
+    }
+}
