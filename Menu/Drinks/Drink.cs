815ffc9a34cda74ef1f305b00fd742ad5e29a08e
Connor Davis <connormdavis99@users.campus>
2019-09-23 16:35:28 -0500

    Added Drink.cs

---
+++ Menu/Drinks/Drink.cs
@@ -0,0 +1,48 @@
+﻿using DinoDiner.Menu.Sides;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    /// <summary>
+    /// Abstract drink class for creating drink objects types
+    /// </summary>
+    public abstract class Drink
+    {
+        private List<string> ingredients = new List<string>();
+
+        /// <summary>
+        /// Price of the drink
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Calories in the drink
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// Ingredients for the drinks
+        /// </summary>
+        public List<string> Ingredients { get => ingredients; }
+
+        /// <summary>
+        /// Size of the drink
+        /// </summary>
+        public Size Size { get; set; }
+
+        /// <summary>
+        /// If the drink contains ice
+        /// </summary>
+        public bool Ice { get; set; }
+
+        /// <summary>
+        /// Removes ice from the drink
+        /// </summary>
+        public void HoldIce()
+        {
+            Ice = false;
+        }
+    }
+}
