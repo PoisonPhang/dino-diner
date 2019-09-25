467343876a1853ddb22a080ac62e490886668293
Connor Davis <connormdavis99@ksu.edu>
2019-09-25 14:16:11 -0500

    Finished Sodasaurus impl

--- Menu/Drinks/Sodasaurus.cs
+++ Menu/Drinks/Sodasaurus.cs
@@ -5,8 +5,14 @@ using DinoDiner.Menu.Sides;
 
 namespace DinoDiner.Menu.Drinks
 {
+    /// <summary>
+    /// Sodasaurus Drink object
+    /// </summary>
     public class Sodasaurus : Drink
     {
+        /// <summary>
+        /// Size property of the Sodasaurus instance
+        /// </summary>
         public override Size Size
         {
             get => size;
@@ -21,7 +27,7 @@ namespace DinoDiner.Menu.Drinks
                         break;
                     case Size.Medium:
                         this.Price = 2.00;
-                        this.Calories = 112;
+                        this.Calories = 156;
                         break;
                     case Size.Small:
                         this.Price = 1.50;
@@ -30,8 +36,16 @@ namespace DinoDiner.Menu.Drinks
                 }
             }
         }
+
+        /// <summary>
+        /// (SodasaurusFlavor) Flavor property of the Sodasaurus instance
+        /// </summary>
         public SodasaurusFlavor Flavor { get; set; }
 
+        /// <summary>
+        /// Constructs a new instance of Sodasaurus
+        /// </summary>
+        /// <param name="flavor"></param>
         public Sodasaurus(SodasaurusFlavor flavor)
         {
             this.Price = 1.50;
