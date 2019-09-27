f8c329fa6aeeb2c10e959e2e80e5957c98f41e61
Connor Davis <connormdavis99@users.campus>
2019-09-27 16:18:22 -0500

    Added Combo class

---
+++ Menu/CretaceousCombo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Entrees;
+using DinoDiner.Menu.Drinks;
+using DinoDiner.Menu.Sides;
+
+namespace DinoDiner.Menu
+{
+    class CretaceousCombo
+    {
+        private Size size =  Size.Small;
+        private Side side;
+        private Drink drink;
+
+        public Entree Entree { get; set; }
+        public Drink Drink
+        {
+            get => drink;
+            set
+            {
+                drink = value;
+                drink.Size = size;
+            }
+        }
+        public Side Side
+        {
+            get => side;
+            set
+            {
+                side = value;
+                side.Size = size;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return Entree.Price + Side.Price + Drink.Price - 0.25;
+            }
+        }
+        public uint Calories
+        {
+            get
+            {
+                return Entree.Calories + Side.Calories + Drink.Calories;
+            }
+        }
+        public Size Size
+        {
+            set
+            {
+                Side.Size = value;
+                Drink.Size = value;
+                size = value;
+            }
+        }
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+                ingredients.AddRange(Entree.Ingredients);
+                ingredients.AddRange(Side.Ingredients);
+                ingredients.AddRange(Drink.Ingredients);
+                return ingredients;
+            }
+        }
+
+        public CretaceousCombo(Entree entree)
+        {
+            Entree = entree;
+            Drink = new JurrasicJava();
+            Side = new Fryceritops();
+        }
+    }
+}
