b6afd1f25f6239f4b80aad84ce79c7f6b272a007
Connor Davis <connormdavis99@ksu.edu>
2019-09-28 11:43:34 -0500

    Compleated SodasaurusTest

--- MenuTest/Drinks/SodasaurusTest.cs
+++ MenuTest/Drinks/SodasaurusTest.cs
@@ -16,11 +16,59 @@ namespace MenuTest.Drinks
         // The ability to set each possible flavor
 
         [Fact]
+        public void ShouldBeAbleToSetFlavorToCola()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Flavor = SodasaurusFlavor.Cola;
+            Assert.Equal(SodasaurusFlavor.Cola, sodasaurus.Flavor);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetFlavorToOrange()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Flavor = SodasaurusFlavor.Orange;
+            Assert.Equal(SodasaurusFlavor.Orange, sodasaurus.Flavor);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetFlavorToVanilla()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Flavor = SodasaurusFlavor.Vanilla;
+            Assert.Equal(SodasaurusFlavor.Vanilla, sodasaurus.Flavor);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetFlavorToChocolate()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Flavor = SodasaurusFlavor.Chocolate;
+            Assert.Equal(SodasaurusFlavor.Chocolate, sodasaurus.Flavor);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetFlavorToRootBeer()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Flavor = SodasaurusFlavor.RootBeer;
+            Assert.Equal(SodasaurusFlavor.RootBeer, sodasaurus.Flavor);
+        }
+
+        [Fact]
         public void ShouldBeAbleToSetFlavorToCherry()
         {
             Sodasaurus sodasaurus = new Sodasaurus();
             sodasaurus.Flavor = SodasaurusFlavor.Cherry;
-            Assert.Equal(SodasaurusFlavor.Cherry, sodasaurus.Flavor); // Specifying type in expected value negates the need for template call
+            Assert.Equal(SodasaurusFlavor.Cherry, sodasaurus.Flavor);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToSetFlavorToLime()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Flavor = SodasaurusFlavor.Lime;
+            Assert.Equal(SodasaurusFlavor.Lime, sodasaurus.Flavor);
         }
 
         // The correct default price, calories, ice, and size
@@ -71,5 +119,58 @@ namespace MenuTest.Drinks
             Assert.Equal(2.00, sodasaurus.Price);
         }
 
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingLarge()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Size = Size.Large;
+            Assert.Equal(2.50, sodasaurus.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingSmall()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Size = Size.Small;
+            Assert.Equal<uint>(112, sodasaurus.Calories);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingMedium()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Size = Size.Medium;
+            Assert.Equal<uint>(156, sodasaurus.Calories);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingLarge()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.Size = Size.Large;
+            Assert.Equal<uint>(208, sodasaurus.Calories);
+        }
+
+        // That invoking HoldIce() results in the Ice property being false.
+
+        [Fact]
+        public void ShouldHaveIcePropertyReturnFalseAfterCall()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            sodasaurus.HoldIce();
+            Assert.False(sodasaurus.Ice);
+        }
+
+        // The correct ingredients are given.
+
+        [Fact]
+        public void ShouldReturnCorrectIngredients()
+        {
+            Sodasaurus sodasaurus = new Sodasaurus();
+            Assert.Contains("Water", sodasaurus.Ingredients);
+            Assert.Contains("Natural Flavors", sodasaurus.Ingredients);
+            Assert.Contains("Cane Sugar", sodasaurus.Ingredients);
+            Assert.Equal(3, sodasaurus.Ingredients.Count);
+        }
     }
 }
