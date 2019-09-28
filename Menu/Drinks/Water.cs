14f3c3a40c2b13f1172d37cb184853a2c648ebcb
Connor Davis <connormdavis99@ksu.edu>
2019-09-28 10:49:41 -0500

    Added Water.cs

---
+++ Menu/Drinks/Water.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Water.cs
+ * Author: Connor Davis
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    /// <summary>
+    /// Water Drink object
+    /// </summary>
+    class Water : Drink
+    {
+        /// <summary>
+        /// Size of this instance of Water
+        /// </summary>
+        public override Size Size
+        {
+            get => size;
+            set => size = value;
+        }
+
+        /// <summary>
+        /// If this instance of Water contains Lemon
+        /// </summary>
+        public bool Lemon { get; set; } = false;
+
+        /// <summary>
+        /// Creates a new instance of Water
+        /// </summary>
+        public Water()
+        {
+            Price = 0.10;
+            Calories = 0;
+            Ingredients.Add("Water");
+            Size = Size.Small;
+        }
+
+        /// <summary>
+        /// Adds Lemon to this instance of Water
+        /// </summary>
+        public void AddLemon()
+        {
+            Ingredients.Add("Lemon");
+        }
+    }
+}
