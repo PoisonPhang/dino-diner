5d2657acd825bb20b7fcdac1a539cb56b7b6c2bc
Connor Davis <connormdavis99@ksu.edu>
2019-10-04 18:25:56 -0500

    Milestone 4

--- Menu/IMenuItem.cs
+++ Menu/IMenuItem.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace DinoDiner.Menu
 {
+    /// <summary>
+    /// Generic menu item interface
+    /// </summary>
     public interface IMenuItem
     {
         /// <summary>
--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -4,23 +4,118 @@ using System.Text;
 
 namespace DinoDiner.Menu
 {
+    /// <summary>
+    /// Menu object containing properties for all available menu items
+    /// </summary>
     public class Menu
     {
+        /// <summary>
+        /// All menu items
+        /// </summary>
         List<IMenuItem> AvailableMenuItems
         {
             get
             {
                 return new List<IMenuItem>()
                 {
+                    new JurassicJava(),
                     new Sodasaurus(),
+                    new Tyrannotea(),
+                    new Water(),
+                    new Brontowurst(),
+                    new DinoNuggets(),
+                    new PrehistoricPBJ(),
+                    new PterodactylWings(),
                     new SteakosaurusBurger(),
-                    new Fryceritops()
+                    new TRexKingBurger(),
+                    new VelociWrap(),
+                    new Fryceritops(),
+                    new MeteorMacAndCheese(),
+                    new MezzorellaSticks(),
+                    new Triceritots(),
+                    new CretaceousCombo(new Brontowurst()),
+                    new CretaceousCombo(new DinoNuggets()),
+                    new CretaceousCombo(new PrehistoricPBJ()),
+                    new CretaceousCombo(new PterodactylWings()),
+                    new CretaceousCombo(new SteakosaurusBurger()),
+                    new CretaceousCombo(new TRexKingBurger()),
+                    new CretaceousCombo(new VelociWrap())
                 };
             }
         }
-        List<IMenuItem> AvailableEntrees { get; }
-        List<IMenuItem> AvailableSides { get; }
-        List<IMenuItem> AvailableDrinks { get; }
-        List<IMenuItem> AvailableCombos { get; }
+
+        /// <summary>
+        /// All Entrees
+        /// </summary>
+        List<IMenuItem> AvailableEntrees
+        {
+            get
+            {
+                return new List<IMenuItem>()
+                {
+                    new Brontowurst(),
+                    new DinoNuggets(),
+                    new PrehistoricPBJ(),
+                    new PterodactylWings(),
+                    new SteakosaurusBurger(),
+                    new TRexKingBurger(),
+                    new VelociWrap()
+                };
+            }
+        }
+
+        /// <summary>
+        /// All Sides
+        /// </summary>
+        List<IMenuItem> AvailableSides
+        {
+            get
+            {
+                return new List<IMenuItem>()
+                {
+                    new Fryceritops(),
+                    new MeteorMacAndCheese(),
+                    new MezzorellaSticks(),
+                    new Triceritots(),
+                };
+            }
+        }
+
+        /// <summary>
+        /// All Drinks
+        /// </summary>
+        List<IMenuItem> AvailableDrinks
+        {
+            get
+            {
+                return new List<IMenuItem>()
+                {
+                    new JurassicJava(),
+                    new Sodasaurus(),
+                    new Tyrannotea(),
+                    new Water()
+                };
+            }
+        }
+
+        /// <summary>
+        /// All Combos
+        /// </summary>
+        List<IMenuItem> AvailableCombos
+        {
+            get
+            {
+                return new List<IMenuItem>()
+                {
+                    new CretaceousCombo(new Brontowurst()),
+                    new CretaceousCombo(new DinoNuggets()),
+                    new CretaceousCombo(new PrehistoricPBJ()),
+                    new CretaceousCombo(new PterodactylWings()),
+                    new CretaceousCombo(new SteakosaurusBurger()),
+                    new CretaceousCombo(new TRexKingBurger()),
+                    new CretaceousCombo(new VelociWrap())
+                };
+            }
+        }
     }
 }
