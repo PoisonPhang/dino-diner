4f9328c3225398c3969160953387fc9e76d298dc
Connor Davis <connormdavis99@ksu.edu>
2019-10-31 13:48:34 -0500

    Custom Menus 2/*

---
+++ PointOfSale/CustomizeDinoNuggets.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DinoDiner.Menu;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for CustomizeDinoNuggets.xaml
+    /// </summary>
+    public partial class CustomizeDinoNuggets : Page
+    {
+        private DinoNuggets dinoNuggets;
+
+        /// <summary>
+        /// Constructs a new instance of CustomizeDinoNuggets with an existing DinoNuggets
+        /// </summary>
+        /// <param name="dinoNuggets">Existing DinoNuggets</param>
+        public CustomizeDinoNuggets(DinoNuggets dinoNuggets)
+        {
+            InitializeComponent();
+            this.dinoNuggets = dinoNuggets;
+        }
+
+        private void OnAddNugget(object sender, RoutedEventArgs args)
+        {
+            dinoNuggets.AddNugget();
+        }
+
+        private void OnDone(object sender, RoutedEventArgs args)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
